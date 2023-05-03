a5c87db45434512583cf4790ae5bd22d07ac4295
Dann013 <125938760+Dann013@users.noreply.github.com>
2023-05-02 22:50:29 -0600

    Add files via upload

---
+++ CajaSinGenericos.java
@@ -0,0 +1,36 @@
+
+package Genericos;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Gateway
+ */
+public class CajaSinGenericos {
+    Object[]content;
+
+    public CajaSinGenericos() {
+    }
+
+    public CajaSinGenericos(Object[] content) {
+        this.content = content;
+    }
+
+    public void add(int pos, Object objeto){
+        content[pos] =objeto;
+    }
+    
+    public Object get (int pos){
+        return content[pos];
+    }
+
+    @Override
+    public String toString() {
+        return "CajaSinGenericos{" + "content=" + Arrays.toString(content) + '}';
+    }
+    
+    
+    
+    
+}
